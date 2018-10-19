34e8a8b3bee41a5f854d4fb0c9c8a527b3281494
Scott Little <scott.little@loop.colum.edu>
2018-10-19 16:04:44 -0500

    Add Most Of The Scripts
    - Added PlayerController
    - Added ComeraControl
    - Added most of the pickup script

---
+++ Assets/Scripts/Overhead2DPlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overhead2DPlayerController : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+
+    private float moveHorizontal, moveVertical;
+
+    int scoreCounter;
+
+    [SerializeField]
+    Text score;
+    [SerializeField]
+    float moveSpeed;
+
+    private void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        myRigidBody.AddForce(movement * moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Pickup")
+        {
+            collision.gameObject.SetActive(false);
+            score.text = scoreCounter++.ToString();
+        }            
+    }
+}
