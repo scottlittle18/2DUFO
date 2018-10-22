38521567c83e36809cd563ad3acc7af103330bb2
Scott Little <scott.little@loop.colum.edu>
2018-10-22 17:38:37 -0500

    Add Main Menu
    - Added Main Menu, but having trouble with the button script

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    int currentScene;
+	
+    // Use this for initialization
+	void Start () {
+       currentScene = SceneManager.GetActiveScene().buildIndex;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PressStartButton()
+    {
+        SceneManager.LoadScene(currentScene +1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ExitCredits()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
